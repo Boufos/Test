af9c85e78cc2cefb0747fda195e796b2d70b325b
Boufos <70595875+Boufos@users.noreply.github.com>
2020-09-19 15:50:42 +0300

    Add files via upload

---
+++ lb_1A/lb_1A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lb_1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personal information (task № 1)\n\n" +
+             "\tLaboratory work № 1\n" +
+            "\tFull name: Teremyaeva O.V. \n" +
+            "\tDate of birth: 07/30/2000\n" +
+            "\tLocation: Armavir\n" +
+            "\tFavorite subject at school: computer science \n" +
+            "\tHobby: drawing, reading\n");
+
+            //Se = w111 + bt - x +y *w
+            int will, bt, x, y, w;
+            will = w = 5;
+            bt = 2;
+            x = y = 4;
+            Console.WriteLine($"Calculate the formula(task number 2)\n\n" +
+                $"\tDefault formula: Se = w111 + bt - x +y *w;\n" +
+                $"\tSe = {will} + {bt} - {x} + {y} * {w};\n" +
+                $"\tSe = {will + bt - x +y *w};\n");
+        }
+    }
+}
