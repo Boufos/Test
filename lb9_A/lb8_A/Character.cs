1449c2e30d6deb8bd7114e915d9670a298cd0558
Boufos <70595875+Boufos@users.noreply.github.com>
2020-09-23 12:43:27 +0300

    Add files via upload

---
+++ lb9_A/lb8_A/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lb9_A
+{
+    class Character
+    {
+		protected String name;
+		protected String activities;
+		protected int health;
+
+		public Character(String name, int health)
+		{
+			this.name = name;
+			this.health = health;
+			this.activities = "has been created";
+		}
+
+		public virtual void GetInfo()
+		{
+			Console.WriteLine($"{name} {activities}, his health is {health}\n");
+		}
+	}
+}
---
+++ lb9_A/lb8_A/ISpeak.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lb9_A
+{
+    interface ISpeak
+    {
+        void Speak();
+    }
+}
