f27b0c3b0f2e7fa6868502ee352c1aa256d0d964
Boufos <70595875+Boufos@users.noreply.github.com>
2020-09-24 12:31:53 +0300

    Add files via upload

---
+++ lb11_A/lb11_A/Employer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lb11_A
+{
+ 
+    class Employer
+    {
+        String ID { get; set; }
+        public String Lname{ get; set; }
+        public String Fname { get; set; }
+        public String Email { get; set; }
+        public float Salary { get; set; }
+        public Boolean Skills{ get; set; }
+        public float Tax { get; set; }
+        // 52-3192486,Sturt,VГ©nus,osturt8 @abc.net.au, $1609.45, Editorial Illustrations,$224.27
+        public static Employer Create (String str)
+        {
+            Employer p = new Employer();
+            string[] e = str.Split(',');
+            p.ID = e[0].Trim();
+            p.Lname = e[2].Trim();
+            p.Fname = e[1].Trim();
+            string Email = e[3].Trim();
+            p.Salary = Convert.ToSingle(e[4].TrimStart('$').Replace('.', ','));
+            String Skills = e[5].Trim();
+            p.Tax = Convert.ToSingle(e[6].TrimStart('$').Replace('.', ','));
+
+            if (string.IsNullOrEmpty(Skills)) p.Skills = false;
+            else p.Skills = true;
+            if (string.IsNullOrEmpty(Email)) p.Email = "Not specified";
+
+            return p;
+        }
+
+        public override string ToString()
+        {
+            String s = string.Format(
+                "**********************************************************************\n" +
+                $"\tID: {ID}, Name: {Fname}, Firstname: {Lname};\n\n" +
+                $"\tEmail: {Email}, Salary: {Salary}, Skiils: {Skills}, Tax: {Tax}\n" +
+                $"**********************************************************************\n");
+
+            return s;
+        }
+
+        //private static String GenderToStr (GenderType g)
+        //{
+        //    if (g == GenderType.Male) return "M";
+        //    return "F";
+        //}
+
+        //private static String HasChildrenToStr(Boolean g)
+        //{
+        //    if (g == true) return "+";
+        //    return "-";
+        //}
+
+    }
+}
---
+++ lb11_A/lb11_A/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lb11_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader f_in = new StreamReader("lr11_09.csv");
+
+#if !DEBUG
+
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"lr11_output.txt");
+            Console.SetOut(new_out);
+#endif
+            List<Employer> all = new List<Employer>();
+
+            try
+            {
+                String line = f_in.ReadLine();
+                while ((line = f_in.ReadLine()) != null)
+                {
+     
+                    all.Add(Employer.Create(line));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine("********************************TASK 1********************************\n" +
+                "\t\t\tEmployee with minimum wage\n");
+            float tSalary = (from p in all select p.Salary - p.Tax).Min();
+            Employer nameEmployerMin = (from p in all where p.Salary - p.Tax == tSalary select p).First();
+            Console.Write(nameEmployerMin);
+
+            Console.WriteLine("\n********************************TASK 2********************************\n" +
+                "\t\tThe number of people without skill\n");
+            int SkillFalse = all.FindAll(p => p.Skills == false).ToList().Count;
+            Console.WriteLine($"Find {SkillFalse} persons");
+
+            Console.WriteLine("\n********************************TASK 3********************************\n" +
+                "\tAverage salary of employees with and without e-mail\n");
+            float EmailFalse = (from p in all where p.Email == "Not specified" select p.Salary).Average();
+            float EmailTrue = (from p in all where p.Email != "Not specified" select p.Salary).Average();
+            Console.WriteLine($"Average salary for employees with e-mail: {EmailTrue}");
+            Console.WriteLine($"Average salary for employees without e-mail: {EmailFalse}");
+
+            Console.WriteLine("\n********************************TASK 4********************************\n" +
+                "\t\t\tTotal tax amount\n");
+            float sumTax = (from p in all select p.Tax).Sum();
+            Console.WriteLine($"Total tax amount: {sumTax}");
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#endif
+        }
+    }
+}
